74e6d2304349f6cd5e2d1fa71826d9aced4bb30c
Dev Raj <devcuber45@gmail.com>
2026-04-10 23:19:43 +0530

    Add asynchronous chai brewing and URL fetching examples

---
+++ Python/Asyncio/async_two.py
@@ -0,0 +1,15 @@
+import asyncio
+import time
+async def brew(name):
+    print(f"Brewing {name}...")
+    await asyncio.sleep(3)
+    print(f" {name} is ready...")
+    
+async def main():
+    await asyncio.gather(
+        brew("Masala Chai"),
+        brew("Ginger Chai"),
+        brew("Milk Chai"),
+        brew("Green Chai")
+    )
+asyncio.run(main())
